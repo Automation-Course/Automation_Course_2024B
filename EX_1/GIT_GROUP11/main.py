507cc7f6d3317672f0e255d7422554c40f7a82f7
YuvalZaf <yuvalzaf@post.bgu.ac.il>
2024-05-19 20:59:31 +0300

    changes for GROUP 11

--- EX_1/GIT_GROUP11/main.py
+++ EX_1/GIT_GROUP11/main.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 from tkinter import messagebox
 
-def Binary():
-    BInary_window = tk.Tk()
+def Binary():# if the user want to convert a binary number
+    BInary_window = tk.Tk() # create GUI
     BInary_window.title("Binary Calculator")
     label = tk.Label(BInary_window, text="Please enter your BINARY number:")
     label.pack(pady=40)
@@ -11,8 +11,8 @@ def Binary():
     button3 = tk.Button(BInary_window, text="GO", command=lambda: convert_to_decimal(entry))
     button3.pack(pady=20, padx=50)
 
-def Decimel():
-    Decimal_window = tk.Tk()
+def Decimel(): #  if the user want to convert decimal number
+    Decimal_window = tk.Tk() #create GUI
     Decimal_window.title("Binary Calculator")
     label = tk.Label(Decimal_window, text="Please enter your Decimal number:")
     label.pack(pady=40)
@@ -21,30 +21,47 @@ def Decimel():
     button3 = tk.Button(Decimal_window, text="GO", command=lambda: decimal_to_binary_fractional(entry))
     button3.pack(pady=20, padx=50)
 
-def convert_to_decimal(entry):
+def convert_to_decimal(entry): #convert binary number to decimal
     decimal = 0.0
-    binary = entry.get()
+    binary = entry.get()#take the value that the user inserted
+    counter = 0
+    for char in binary:#check if theres letters or another numbers beside 0 or 1 in the value
+        if char == '.':#to check if the user inserted too many dots in the value
+            counter += 1
+        if char != '1' and char != '0' and char != '.':# give an error to the user in that case
+            messagebox.showinfo("Error", "error - please enter BINARY number")
+            entry.master.destroy()
+            return
+        if counter > 1:# error if theres too many dots
+            messagebox.showinfo("Error", "ERROR - too many dots")
+            entry.master.destroy()
+            return
     if '.' in binary:
-        integer, fraction = binary.split('.')
-    else:
+        integer, fraction = binary.split('.')#handle with fractions
+        # for integer:
+        length = len(integer) - 1
+        for digit in integer:
+            decimal += int(digit) * (2 ** length)
+            length -= 1
+        # for fraction:
+        i = 1
+        for digit in fraction:
+            decimal += int(digit) * (2 ** -i)
+            i += 1
+    else: # if theres no fractions:
         integer = binary
     #for integer:
-    length = len(integer) - 1
-    for digit in integer:
-        decimal += int(digit) * (2 ** length)
-        length -= 1
-    #for fraction:
-    i=1
-    for digit in fraction:
-        decimal += int(digit) * (2 ** -i)
-        i +=1
-    messagebox.showinfo("Decimal Value", f"The decimal value is: {decimal}")
+        length = len(integer) - 1
+        for digit in integer:
+            decimal += int(digit) * (2 ** length)
+            length -= 1
+    messagebox.showinfo("Decimal Value", f"The decimal value is: {decimal}") #give the answer to the user
     entry.master.destroy()
 
 
-def decimal_to_binary_fractional(entry, precision=5):
-    decimal = float(entry.get())
-    if decimal < 0:
+def decimal_to_binary_fractional(entry, precision=5):#convert decimal to binary number
+    decimal = float(entry.get())#get the value from the user, float to handle fractions
+    if decimal < 0:# if the value is negative
         messagebox.showinfo("Error", "error - please enter positive number")
         entry.master.destroy()
         return
